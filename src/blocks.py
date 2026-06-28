969c6c7d68974f6cb2e030d071cd7236fa1f2d28
klaasOGD <klaas.depater@outlook.com>
2026-06-28 18:07:09 +0200

    Add block to block type function and tests

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,57 @@
+# Test regex patterns here: https://regexr.com/
+import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(block: str) -> BlockType:
+    if len(re.findall(r"^#{1,6}\ .+", block)) > 0 and len(block.split("\n")) == 1:
+        return BlockType.HEADING
+    if len(re.findall(r"^`{3}|`{3}$", block)) == 2:
+        return BlockType.CODE
+    split = block.split("\n")
+    quote = False
+    for s in split:
+        if s.startswith(">"):
+            quote = True
+        else:
+            quote = False
+            break
+    if quote:
+        return BlockType.QUOTE
+    unordered_list = False
+    for s in split:
+        if s.startswith("- "):
+            unordered_list = True
+        else:
+            unordered_list = False
+            break
+    if unordered_list:
+        return BlockType.UNORDERED_LIST
+    ordered_list = False
+    previous_digit = None
+    for s in split:
+        matches = re.findall(r"^(\d+)\.\ ", s)
+        if len(matches) == 0:
+            ordered_list = False
+            break
+        current_digit = int(matches[0])
+        if previous_digit:
+            if current_digit != previous_digit + 1:
+                ordered_list = False
+                break
+        ordered_list = True
+        previous_digit = current_digit
+    if ordered_list:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = []
     split = markdown.split("\n\n")
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdownToBlocks(unittest.TestCase):
         def test_markdown_to_blocks(self):
@@ -66,3 +66,48 @@ This is the same paragraph on a new line
                     "- This is a list\n- with items\n- and whitespace",
                 ],
             )
+
+class TestBlockToBlockType(unittest.TestCase):
+     def test_paragraph(self):
+        block = "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+     def test_heading(self):
+        block = "### Heading three"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+     def test_heading_with_linefeed(self):
+        block = "### Heading three\nsome other test"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+     def test_code(self):
+        block = "```\n# This is a code block\nwith:\n    some(code)\n```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+     def test_quote(self):
+        block = "> This is a quote block\n> With a nice quote"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+     def test_quote_no_space(self):
+        block = ">This is a quote block\n>With a nice quote"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+     def test_unordered_list(self):
+        block = "- This is a list block\n- With no order"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+     def test_ordered_list(self):
+        block = "1. This is a list block\n2. With an order\n3. ..."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+     def test_ordered_list_not_starting_at_one(self):
+        block = "328. This is a list block\n329. With an order\n330. ..."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+     def test_ordered_list_out_of_order(self):
+        block = "2. This is a list block\n1. With a wrong order\n3. ..."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+if __name__ == "__main__":
+    unittest.main()
