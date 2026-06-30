f7f6ecadb5b781572fedfc8497abe94affe26649
klaasOGD <klaas.depater@outlook.com>
2026-06-30 20:11:40 +0200

    Generate pages recursively

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from textnode import *
 import os
 import shutil
+from pathlib import Path
+
+from textnode import *
 from markdown_to_html import markdown_to_html_node
 from extract_title import extract_title
-from pathlib import Path
 
 def copy_content(src_path="static/", dst_path="public/"):
     if os.path.exists(dst_path):
@@ -15,13 +16,13 @@ def copy_content(src_path="static/", dst_path="public/"):
         joined_src_path = os.path.join(src_path, item)
         joined_dst_path = os.path.join(dst_path, item)
         if os.path.isfile(joined_src_path):
-            print(f"Copying {joined_src_path} to {joined_dst_path}")
+            print(f"Copying {joined_src_path} -> {joined_dst_path}")
             shutil.copy(joined_src_path, joined_dst_path)
         else:
             copy_content(joined_src_path, joined_dst_path)
 
 def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    print(f"Generating page: {from_path} -> {dest_path} using {template_path}")
     with open(from_path, 'r') as content_file:
         md_content = content_file.read()
     with open(template_path, 'r') as template_file:
@@ -33,13 +34,21 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as dest_file:
         dest_file.write(filled_template)
 
+def generate_pages_recursively(dir_path_content="content/", template_path="template.html", dest_dir_path="public/"):
+    items = os.listdir(dir_path_content)
+    for item in items:
+        joined_src_path = os.path.join(dir_path_content, item)
+        joined_dst_path = os.path.join(dest_dir_path, item)
+        if os.path.isfile(joined_src_path):
+            joined_dst_path = joined_dst_path.removesuffix(".md")
+            joined_dst_path += ".html"
+            generate_page(joined_src_path, template_path, joined_dst_path)
+        else:
+            generate_pages_recursively(joined_src_path, template_path, joined_dst_path)
+
 def main():
     copy_content()
-    generate_page("content/index.md", "template.html", "public/index.html")
-    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
-    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
-    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
-    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
+    generate_pages_recursively()
 
 if __name__ == "__main__":
     main()
