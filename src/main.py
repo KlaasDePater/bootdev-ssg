2082a10d371b0290ae8ea82f363ff3d07394340a
klaasOGD <klaas.depater@outlook.com>
2026-07-01 21:52:02 +0200

    Add build script, build in docs, remove public

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from pathlib import Path
+import sys
 
 from textnode import *
 from markdown_to_html import markdown_to_html_node
@@ -21,7 +22,7 @@ def copy_content(src_path="static/", dst_path="public/"):
         else:
             copy_content(joined_src_path, joined_dst_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page: {from_path} -> {dest_path} using {template_path}")
     with open(from_path, 'r') as content_file:
         md_content = content_file.read()
@@ -29,12 +30,12 @@ def generate_page(from_path, template_path, dest_path):
         template = template_file.read()
     html_content = markdown_to_html_node(md_content).to_html()
     title = extract_title(md_content)
-    filled_template = template.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    filled_template = template.replace("{{ Title }}", title).replace("{{ Content }}", html_content).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     Path(dest_path).parent.mkdir(parents=True, exist_ok=True)
     with open(dest_path, "w") as dest_file:
         dest_file.write(filled_template)
 
-def generate_pages_recursively(dir_path_content="content/", template_path="template.html", dest_dir_path="public/"):
+def generate_pages_recursively(dir_path_content="content/", template_path="template.html", dest_dir_path="public/", basepath="/"):
     items = os.listdir(dir_path_content)
     for item in items:
         joined_src_path = os.path.join(dir_path_content, item)
@@ -42,13 +43,21 @@ def generate_pages_recursively(dir_path_content="content/", template_path="templ
         if os.path.isfile(joined_src_path):
             joined_dst_path = joined_dst_path.removesuffix(".md")
             joined_dst_path += ".html"
-            generate_page(joined_src_path, template_path, joined_dst_path)
+            generate_page(joined_src_path, template_path, joined_dst_path, basepath)
         else:
-            generate_pages_recursively(joined_src_path, template_path, joined_dst_path)
+            generate_pages_recursively(joined_src_path, template_path, joined_dst_path, basepath)
 
 def main():
+    arguments = sys.argv
+    if len(arguments) == 2:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    if not basepath.endswith("/"):
+        raise Exception(f"Basepath {basepath} does not end with a '/'")
+    print(f"Basepath will be: {basepath}")
     copy_content()
-    generate_pages_recursively()
+    generate_pages_recursively("content/", "template.html", "docs/", basepath)
 
 if __name__ == "__main__":
     main()
